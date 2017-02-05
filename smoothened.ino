44ee2e047989214371d3b63aaf3c151f5780d66c
Aditya Rathi <adityamrathi1@gmail.com>
2017-02-05 13:53:56 +0530

    Add files via upload

---
+++ smoothened.ino
@@ -0,0 +1,72 @@
+/*
+ ADXL3xx
+
+ Reads an Analog Devices ADXL3xx accelerometer and communicates the
+ acceleration to the computer.  The pins used are designed to be easily
+ compatible with the breakout boards from Sparkfun, available from:
+ http://www.sparkfun.com/commerce/categories.php?c=80
+
+ http://www.arduino.cc/en/Tutorial/ADXL3xx
+
+ The circuit:
+ analog 0: accelerometer self test
+ analog 1: z-axis
+ analog 2: y-axis
+ analog 3: x-axis
+ analog 4: ground
+ analog 5: vcc
+
+ created 2 Jul 2008
+ by David A. Mellis
+ modified 30 Aug 2011
+ by Tom Igoe
+
+ This example code is in the public domain.
+
+*/
+
+// these constants describe the pins. They won't change:
+const int groundpin = A4;             // analog input pin 4 -- ground
+const int powerpin = A5;              // analog input pin 5 -- voltage
+const int xpin = A3;                  // x-axis of the accelerometer
+const int ypin = A2;                  // y-axis
+const int zpin = A1;                  // z-axis (only on 3-axis models)
+
+void setup() {
+  // initialize the serial communications:
+  Serial.begin(19600);
+
+  // Provide ground and power by using the analog inputs as normal
+  // digital pins.  This makes it possible to directly connect the
+  // breakout board to the Arduino.  If you use the normal 5V and
+  // GND pins on the Arduino, you can remove these lines.
+  pinMode(groundpin, OUTPUT);
+  pinMode(powerpin, OUTPUT);
+  digitalWrite(groundpin, LOW);
+  digitalWrite(powerpin, HIGH);
+}
+int a;
+int b;
+int c;
+void loop() {
+  // print the sensor values:
+  a=0;
+  b=0;
+  c=0;
+  for(int i=0;i<64;i++)
+  {
+  a=a+analogRead(xpin);
+  b=b+analogRead(ypin);
+  c=c+analogRead(zpin);
+  }
+  a=a/64;
+  b=b/64;
+  c=c/64;
+  Serial.print(a);
+  Serial.print("\t");
+  Serial.print(b);
+  Serial.print("\t");
+  Serial.print(c);
+  Serial.print("\t");
+  Serial.print("\n");
+}
---
+++ working_WASD.pde
@@ -0,0 +1,83 @@
+import processing.serial.*;
+import java.awt.Robot;
+import java.awt.AWTException;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+Serial myPort;
+Robot robot;
+String val;
+int x;
+int y;
+int z;
+int p=0;
+int q=0;
+int t=0;
+int s=0;
+
+void setup() {
+  String portName = Serial.list()[0]; //change the 0 to a 1 or 2 etc. to match your port
+  myPort = new Serial(this, portName, 19600);
+  try { 
+    robot = new Robot();
+  } catch (AWTException e) {
+    e.printStackTrace();
+    exit();
+  }
+}
+
+void draw()
+{
+  if ( myPort.available() > 0) 
+  {  // If data is available,
+  val = myPort.readStringUntil('\n'); // read it and store it in val
+  int[] nums = int(split(val, '\t'));
+  x=nums[0];
+  y=nums[1];
+  z=nums[2];
+  if(x>355)
+  {
+    robot.keyPress(KeyEvent.VK_W);
+    p=1;
+  }
+  else
+  {
+    if(p==1)
+    {
+    robot.keyRelease(KeyEvent.VK_W);
+  }}
+  if(x<300)
+  {
+    robot.keyPress(KeyEvent.VK_S);
+    q=1;
+  }
+  else
+  {
+    if(q==1)
+    {
+    robot.keyRelease(KeyEvent.VK_S);
+  }}
+  }
+  if(y>360)
+  {
+    robot.keyPress(KeyEvent.VK_A);
+    s=1;
+  }
+  else
+  {
+    if(s==1)
+    {
+    robot.keyRelease(KeyEvent.VK_A);
+  }}
+  if(y<310)
+  {
+    robot.keyPress(KeyEvent.VK_D);
+    t=1;
+  }
+  else
+  {
+    if(t==1)
+    {
+    robot.keyRelease(KeyEvent.VK_D);
+  }}
+ 
+}
